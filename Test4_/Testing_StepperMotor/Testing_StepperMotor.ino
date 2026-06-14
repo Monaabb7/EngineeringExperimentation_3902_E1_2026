dd17b68372fa0b6127ab0c4a2a417ba14091112c
Monaabb7 <mblake@wpi.edu>
2026-06-15 01:00:25 +0800

    Update Testing_StepperMotor.ino

--- Test4_/Testing_StepperMotor/Testing_StepperMotor.ino
+++ Test4_/Testing_StepperMotor/Testing_StepperMotor.ino
@@ -1,9 +1,33 @@
+#include <Stepper.h>
+  //1.8 degree step angle = 200 steps per revolution
+
+  const int StepsPerRevolution = 200;
+
+  //Initializing Stepper Library 
+  //(steps,AIN1,AIN2, BIN1,BIN2)
+  Stepper nema17Stepper(StepsPerRevolution,5,4,6,7);
+
 void setup() {
   // put your setup code here, to run once:
+
+  //Set the motor speed in rpm
+  nema17Stepper.setSpeed(10);
+
+  Serial.begin (9600);
+
+  Serial.println("NEMA 17 is initialized with TB6612");
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+  Serial.println("Moving Clockwise");
+  nema17Stepper.step(StepsPerRevolution);
+  delay(1000); //Pause for one second
+
+  Serial.println("Moving CCW");
+  nema17Stepper.step(-StepsPerRevolution);
+  delay(1000);
 
 }
