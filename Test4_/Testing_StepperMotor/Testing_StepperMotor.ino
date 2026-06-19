9f344c5421b815ef5c66bd25bc6d7039bc59025f
Monaabb7 <mblake@wpi.edu>
2026-06-20 07:10:58 +0800

    Testing_Hall_Effect_Sensor
    Testing_Hall_Effect_Sensor

--- Test4_/Testing_StepperMotor/Testing_StepperMotor.ino
+++ Test4_/Testing_StepperMotor/Testing_StepperMotor.ino
@@ -4,8 +4,12 @@
   const int StepsPerRevolution = 200;
 
   //Initializing Stepper Library 
-  //(steps,AIN1,AIN2, BIN1,BIN2)
+  //(steps, AIN1, AIN2, BIN1, BIN2)
   Stepper nema17Stepper(StepsPerRevolution,5,4,6,7);
+
+  int sensor2 =2; // sensor pin
+  int val2; // 0: Magnetic field detected, 1: No magnetic field detected
+
 
 void setup() {
   // put your setup code here, to run once:
@@ -17,17 +21,31 @@ void setup() {
 
   Serial.println("NEMA 17 is initialized with TB6612");
 
+    pinMode(sensor2, INPUT); // set sensor pin as input
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
-  Serial.println("Moving Clockwise");
-  nema17Stepper.step(StepsPerRevolution);
-  delay(1000); //Pause for one second
+  //Serial.println("Moving Clockwise");
+ // nema17Stepper.step(StepsPerRevolution);
+  //delay(1000); //Pause for one second
 
-  Serial.println("Moving CCW");
-  nema17Stepper.step(-StepsPerRevolution);
+  //Serial.println("Moving Counter Clock Wise");
+  //nema17Stepper.step(-StepsPerRevolution);
+  //delay(1000);
+
+  val2 = digitalRead(sensor2); //Read the sensor
+
+  if(val2==1){
+    Serial.println("Magnet Not Detected");
+  }
+  else{
+    Serial.println("Magnet Detected");
+  }
+
+  Serial.print("ky003 value: ");
+  Serial.println(val2);
+  
   delay(1000);
-
 }
