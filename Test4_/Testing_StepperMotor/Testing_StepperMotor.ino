7da5bc3a515d192862ffebe24c48b6504082f4d3
Monaabb7 <mblake@wpi.edu>
2026-06-22 10:19:20 +0800

    Improve_Data_Collection_IMU_StepperMotor_HallEffectSensor
    Improve_Data_Collection_IMU_StepperMotor_HallEffectSensor

--- Test4_/Testing_StepperMotor/Testing_StepperMotor.ino
+++ Test4_/Testing_StepperMotor/Testing_StepperMotor.ino
@@ -12,6 +12,13 @@
   //(steps, AIN1, AIN2, BIN1, BIN2)
   Stepper nema17Stepper(StepsPerRevolution,5,4,6,7);
 
+  //Number of rotations at 15 for 3 minutes = 45 rotations
+
+  //Define time in ms
+
+  const long duration = 3*60*1000; //180000 ms (3min)
+
+
   int sensor2 =2; // sensor pin
   int val2; // 0: Magnetic field detected, 1: No magnetic field detected
 
@@ -21,7 +28,7 @@ void setup() {
   // put your setup code here, to run once:
 
   //Set the motor speed in rpm
-  nema17Stepper.setSpeed(30);
+  nema17Stepper.setSpeed(15);
 
   Serial.begin (9600);
 
@@ -47,51 +54,36 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
-  Serial.println("Moving Clockwise");
-  nema17Stepper.step(StepsPerRevolution);
- delay(1000); //Pause for one second
+  Serial.println("3 minute cycle starting");
 
-//   Serial.println("Moving Counter Clock Wise");
-//  nema17Stepper.step(-StepsPerRevolution);
-//  delay(1000);
+  long startTime=millis();
+  Serial.print("Time, Hall Effect, RotX, TotY, Rotz");
 
-  val2 = digitalRead(sensor2); //Read the sensor
+  while (millis()-startTime < duration)
+  {
+  Serial.print(millis());
+   nema17Stepper.step(1);
 
-  if(val2==1){
-    Serial.println("Magnet Not Detected");
-  }
-  else{
-    Serial.println("Magnet Detected");
-  }
+   Serial.print(",");
+   
+   val2 = digitalRead(sensor2); //Read the sensor
 
-  Serial.print("ky003 value: ");
-  Serial.println(val2);
-  
-  delay(1000);
+   Serial.println(val2);
+     Serial.print(",");
 
-  /* Get new sensor events with the readings */
+
+    /* Get new sensor events with the readings */
   sensors_event_t a2, g2, temp2;
   mpu2.getEvent(&a2, &g2, &temp2);
-
-  /* Print out the values */
-  Serial.print("Acceleration X: ");
-  Serial.print(a2.acceleration.x);
-  Serial.print(", Y: ");
-  Serial.print(a2.acceleration.y);
-  Serial.print(", Z: ");
-  Serial.print(a2.acceleration.z);
-  Serial.println(" m/s^2");
-
-  Serial.print("Rotation X: ");
+  Serial.print(",");
   Serial.print(g2.gyro.x);
-  Serial.print(", Y: ");
+  Serial.print(",");
   Serial.print(g2.gyro.y);
-  Serial.print(", Z: ");
-  Serial.print(g2.gyro.z);
-  Serial.println(" rad/s");
+  Serial.print(",");
+  Serial.println(g2.gyro.z);
 
-  Serial.print("Temperature: ");
-  Serial.print(temp2.temperature);
-  Serial.println(" degC");
-  delay(500);
+  }
+ 
+
+ 
 }
